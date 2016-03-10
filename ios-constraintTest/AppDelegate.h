cfa587a0ce446828651cfaa878126846a45a6d00
caa1211 <caa@livemail.tw>
2016-03-10 20:27:47 +0800

    Initial Commit

---
+++ ios-constraintTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-constraintTest
+//
+//  Created by caa on 3/10/16.
+//  Copyright © 2016 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-constraintTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-constraintTest
+//
+//  Created by caa on 3/10/16.
+//  Copyright © 2016 yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
